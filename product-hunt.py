c9a1161068179751c55274e8e9ee1fc1d38e683d
prateek-ghanghas <139649603+prateek-ghanghas@users.noreply.github.com>
2023-07-17 00:00:54 +0530

    product-hunt code uploaded
    parser created to get "product_name", "product_url", "upvote_count" and "description"

---
+++ product-hunt.py
@@ -0,0 +1,48 @@
+import requests
+from bs4 import BeautifulSoup as bs
+import json
+
+URL = "https://www.producthunt.com/"
+
+response = requests.get(URL)
+
+html = bs(response.content, 'html5lib')
+
+#print(html.prettify())
+
+tags = html.find('script', attrs = {'id':"__NEXT_DATA__"}).string.strip()
+
+data = json.loads(tags)
+
+#print(data)
+
+data = data["props"]["apolloState"]
+
+ref = []
+
+for i in data:
+        if i == "HomefeedPageFEATURED-0-31":
+            for j in range(len(data[i]["items"])):
+                ref.append(data[i]["items"][j]["__ref"])
+
+info = []
+
+try:
+        for k in ref:
+                maindic = {}
+                dic = {}
+                if "Ad" in k or "Discussion" in k or "Antholo" in k or "Collection" in k:
+                      continue
+                slug = data[k]["slug"]
+                dic["name"] = data[k]["name"]
+                dic["tagline"] = data[k]["tagline"]
+                dic["votesCount"] = data[k]["votesCount"]
+                dic["link"] = "https://www.producthunt.com/posts/" + slug
+                maindic.update(dic)
+                info.append(maindic)
+except KeyError:
+      pass
+
+print(info)
+
+
